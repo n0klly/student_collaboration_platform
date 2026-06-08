b14ced5672e5e07f21b18310821bd8b56a9925e0
Nikolai <noklly.main@gmail.com>
2026-06-08 10:17:55 +0300

    Создали проект

---
+++ src/main/java/noklly/project/student_collaboration_platform/StudentCollaborationPlatformApplication.java
@@ -0,0 +1,13 @@
+package noklly.project.student_collaboration_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCollaborationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCollaborationPlatformApplication.class, args);
+    }
+
+}
